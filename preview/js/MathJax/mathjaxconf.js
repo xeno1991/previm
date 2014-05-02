41d217f8a6f95f6114e40d99f01d1f83dc19c79d
Ryo Murakami <integral14@gmail.com>
2014-05-03 00:43:37 +0900

    式番号のnumberingを直した，

--- preview/js/MathJax/mathjaxconf.js
+++ preview/js/MathJax/mathjaxconf.js
@@ -2,7 +2,22 @@ MathJax.Hub.Config({
     tex2jax: {
         inlineMath: [['$','$'], ["\\(","\\)"]] 
     },
-    TeX: {
-        equationNumbers: { autoNumber: "AMS" } //automatic equation numbering
+TeX: {
+    equationNumbers: { autoNumber: "AMS" }, //automatic equation numbering
+    Macros:{
+        grad:   ['\\mathrm{grad}\\,'],
+        diver:  ['\\mathrm{div}\\,'],
+        rot:    ['\\mathrm{rot}\\,'],
+        bm:     ['{\\boldsymbol{#1}}', 1],
     }
+}
 });
+
+// numberingを初期化する関数
+// Reprocessする前に呼ぶ
+function resetEquationNumbers(){
+    var AMS = MathJax.Extension["TeX/AMSmath"];
+    AMS.startNumber = 0;
+    AMS.labels = {};
+}
+
